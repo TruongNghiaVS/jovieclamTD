83fad514c9678f0a7c8bba94117d8fec3f06cde6
Thanh Minh & Hoan <90053384+thanhminh1510@users.noreply.github.com>
2023-12-21 11:11:39 +0700

    check ads

--- resources/views/templates/employers/job/inc/ads.blade.php
+++ resources/views/templates/employers/job/inc/ads.blade.php
@@ -78,3 +78,89 @@ $ads = \App\AdBanner::all();
 }
 </style>
 @endpush
+
+
+{{--
+<div id="adbanner" class="carousel slide" data-ride="carousel">
+
+    <!-- Indicators -->
+
+    <!-- The slideshow -->
+    <div class="carousel-inner" >
+
+        <div class="row" id="adbanner-side">
+           
+        </div>
+    </div>
+</div>
+<!-- Left and right controls -->
+</div>
+
+@push('styles')
+<link href="{{ asset('/vietstar/css/style.css')}}" rel="stylesheet">
+<style>
+.gad {}
+
+#adbanner,
+.carousel-inner,
+.carousel-inner>.carousel-item {
+    width: 100%;
+    height: 100%;
+}
+
+.carousel-inner>.carousel-item>img,
+.carousel-inner>.carousel-item>a>img {
+    width: 100%;
+    height: 100%;
+    margin: auto;
+}
+</style>
+@endpush
+
+@push('scripts')
+<script type="text/javascript">
+    $(document).ready(function() {
+            // Set your API endpoint
+            const apiUrl = `http://localhost:8001/AdvertisementBannerJob/getAll`;
+            console.log(apiUrl);
+            // Make the API request
+            $.ajax({
+                url: apiUrl,
+                type: 'GET',
+                dataType: 'json',
+                success: function(data) {
+                    // Handle the data from the API
+                    console.log(data);
+
+                    if(data) {
+                        data.forEach(element => {
+                            if (element.priorities != '1') {
+                                $(".carousel-inner #adbanner-side").append(`
+                                    <div class="col-12 col-md-6 col-lg-12 ">
+                                        <div class="item">
+                                            <div class="image loadAds">
+                                                <a href="#">
+                                                    <img src="http://localhost:8001/admin_assets/${element.linkDesktop}"
+                                                        alt="#">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                `);
+                            }
+                        });
+                    }
+                   
+                },
+                error: function(xhr, status, error) {
+                    // Handle errors
+                    console.error('Error:', error);
+                }
+            });
+        });
+
+</script>
+@endpush
+
+
+--}}
