ee6a92c1e4ffadedccf908bac683d38e7c05bd85
Thanh Minh & Hoan <90053384+thanhminh1510@users.noreply.github.com>
2024-01-16 16:17:52 +0700

    change img

--- resources/views/templates/employers/includes/search.blade.php
+++ resources/views/templates/employers/includes/search.blade.php
@@ -2,20 +2,22 @@
     <div class="header-bar-bg">
         <div class="swiper slider-hero-banner">
             @php
-            $sliders = \App\Slider::select([
+            $sliders = \App\Slider::where("type",1)->select([
             'sliders.id', 'sliders.slider_id',
             'sliders.is_active','sliders.used_for','sliders.slider_image','sliders.slider_image_mobile'
             ])->sorted()->get();
             @endphp
             <div class="swiper-wrapper">
                 @foreach($sliders as $slider)
-                @if($slider->is_active)
-                    <div class="swiper-slide p-0">
-                        <div class="is-pc bg-slider" style="background-image: url({{'slider_images/'.$slider->slider_image}});"></div>
-                        <div class="is-sp bg-slider" style="background-image: url({{'slider_images/'.$slider->slider_image_mobile}});"></div>
-                    </div>
-                @endif
+                    @if($slider->is_active)
+
+                        <div class="swiper-slide p-0">
+                            <div class="is-pc bg-slider" style="background-image: url({{'slider_images/'.$slider->slider_image}});"></div>
+                            <div class="is-sp bg-slider" style="background-image: url({{'slider_images/'.$slider->slider_image_mobile}});"></div>
+                        </div>
+                    @endif
                 @endforeach
+            </div>
             </div>
             <div class="swiper-pagination"></div>
         </div>
--- resources/views/templates/vietstar/includes/search.blade.php
+++ resources/views/templates/vietstar/includes/search.blade.php
@@ -1,20 +1,22 @@
 <section class="header-bar">
     <div class="header-bar-bg">
         <div class="swiper slider-hero-banner">
-            @php
-            $sliders = \App\Slider::select([
+        @php
+            $sliders = \App\Slider::where("type",1)->select([
             'sliders.id', 'sliders.slider_id',
             'sliders.is_active','sliders.used_for','sliders.slider_image','sliders.slider_image_mobile'
             ])->sorted()->get();
+            dd($sliders);
             @endphp
             <div class="swiper-wrapper">
                 @foreach($sliders as $slider)
-                @if($slider->is_active)
-                <div class="swiper-slide">
-                    <div class="is-pc bg-slider" style="background-image: url({{'slider_images/'.$slider->slider_image}});"></div>
-                    <div class="is-sp bg-slider" style="background-image: url({{'slider_images/'.$slider->slider_image_mobile}});"></div>
-                </div>
-                @endif
+                    @if($slider->is_active)
+
+                        <div class="swiper-slide p-0">
+                            <div class="is-pc bg-slider" style="background-image: url({{'slider_images/'.$slider->slider_image}});"></div>
+                            <div class="is-sp bg-slider" style="background-image: url({{'slider_images/'.$slider->slider_image_mobile}});"></div>
+                        </div>
+                    @endif
                 @endforeach
             </div>
             <div class="swiper-pagination"></div>
