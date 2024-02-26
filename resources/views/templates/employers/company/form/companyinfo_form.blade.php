57fe2aa1c0c87d3ae0524deff23d61ccb24eabfd
minhdev <minhdev@gmail.com>
2024-02-26 16:01:16 +0700

    make modal center

--- resources/views/templates/employers/company/form/companyinfo_form.blade.php
+++ resources/views/templates/employers/company/form/companyinfo_form.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="company_info" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
+  <div class="modal-dialog modal-lg modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="wizard-title">{{__('Company Information')}}</h5>
