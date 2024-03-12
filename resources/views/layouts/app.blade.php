e853c73765afab27ef16f5a71c5eb3089f5547c9
minhdev <minhdev@gmail.com>
2024-03-12 15:58:06 +0700

    add meta blog

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@ if (!isset($seo)) {
     <title>{{__($seo->seo_title) }}</title>
     <meta name="Description" content="{!! $seo->seo_description !!}">
     <meta name="Keywords" content="{!! $seo->seo_keywords !!}">
+    <meta name="description" content="@yield('description')">
+    <meta name="keywords" content="@yield('keywords')">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
     {!! $seo->seo_other !!}
--- resources/views/templates/vietstar/layouts/app.blade.php
+++ resources/views/templates/vietstar/layouts/app.blade.php
@@ -14,7 +14,8 @@ if (!isset($seo)) {
     <meta name="Description" content="{!! $seo->seo_description !!}">
     <meta name="Keywords" content="{!! $seo->seo_keywords !!}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-
+    <meta name="description" content="@yield('description')">
+    <meta name="keywords" content="@yield('keywords')">
     {!! $seo->seo_other !!}
     <link rel="icon" type="image/x-icon" href="{{ asset('/vietstar/imgs/jobvieclam.png') }}">
 
